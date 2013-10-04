b36177832a65c2e38458ab56edf3dd453fe2310b
Brett Shollenberger <brett.shollenberger@gmail.com>
2013-10-03 23:41:09 -0400

    Add Javascript solution to problem4

---
+++ Problem_4/javascript/lib/problem4.js
@@ -0,0 +1,18 @@
+var _ = require('underscore');
+
+function isPalindrome(num) {
+  return num.toString() == _.toArray(num.toString()).reverse().join('');
+}
+
+var largest_product = 0;
+
+_.chain(_.range(900, 999)).each(function(num) {
+  _.chain(_.range(900, 999)).each(function(num2) {
+    if (isPalindrome(num2 * num)) {
+      largest_product = num2 * num;
+    }
+  })
+})
+
+console.log(largest_product);
+console.log(largest_product == 906609);
